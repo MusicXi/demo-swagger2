840578a05ebed332d9d0ff7103b9542e4ca5f207
MusicXi <792331407@qq.com>
2021-01-06 13:40:14 +0800

    swagger 多文件上传测试

--- src/main/java/hello/controller/TestController.java
+++ src/main/java/hello/controller/TestController.java
@@ -2,16 +2,22 @@ package hello.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.collections.map.LinkedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.Map;
 
 /**
@@ -47,4 +53,68 @@ public class TestController {
         result.put("request.getLocale().toLanguageTag()", request.getLocale().toLanguageTag());
         return result;
     }
+
+    @PostMapping(value = "/upload", consumes = "multipart/*", headers = "content-type=multipart/form-data")
+    @ApiOperation(value = "上传图片", notes = "上传图片", httpMethod = "POST")
+    @ResponseBody
+    public Map<String, Object> upload(@ApiParam(value = "测试图片", required = true) MultipartFile file, @ApiParam(value = "测试图片2", required = true) MultipartFile file2) {
+        Map<String, Object> resultMap = new HashedMap();
+        this.saveFile("test01", file, resultMap);
+        this.saveFile("test02", file2, resultMap);
+        return resultMap;
+    }
+
+    /**
+     * 保存文件
+     * @param name
+     * @param file
+     * @return
+     */
+    private Map<String, Object> saveFile(String name, MultipartFile file, Map<String, Object> resultMap) {
+        if (file.isEmpty()) {
+            resultMap.put(name, "文件没有变化");
+            return resultMap;
+        }
+
+        if (file.getContentType().contains("image")) {
+            try {
+                String temp =   File.separator +  "images" + File.separator + "upload" + File.separator;
+                // 获取图片的文件名
+                String fileName = file.getOriginalFilename();
+                // 获取图片的扩展名
+                String extensionName = fileName.substring(fileName.indexOf("."));
+                // 新的图片文件名 = 获取时间戳+"."图片扩展名
+                String newFileName = name + extensionName;
+                // 数据库保存的目录
+                String datedDirectory = temp.concat(String.valueOf(1)).concat(File.separator);
+                // 文件路径
+                File path = new File(ResourceUtils.getURL("classpath:").getPath());
+                if(!path.exists()) {
+                    path = new File("");
+                }
+
+                String filePath = path.getAbsolutePath().concat(File.separator + "static" + datedDirectory);
+
+                File dest = new File(filePath, newFileName);
+                // 创建文件目录
+                if (!dest.getParentFile().exists()) {
+                    dest.getParentFile().mkdirs();
+                }
+                // 上传到指定目录
+                file.transferTo(dest);
+                Map<String, Object> detail = new HashedMap();
+                LOGGER.info("文件:{} 上传成功!", file.getOriginalFilename());
+                detail.put("originalFilename", fileName);
+                detail.put("url", "localhost:8080" + datedDirectory + newFileName);
+                detail.put("absolutePath", dest.getAbsoluteFile().toString());
+                resultMap.put(name, detail);
+            }catch (Exception e){
+                LOGGER.error("文件上传失败" + e.getMessage(), e);
+                resultMap.put(name, "文件上传失败" + e.getMessage());
+            }
+        }
+        return resultMap;
+
+    }
+
 }
